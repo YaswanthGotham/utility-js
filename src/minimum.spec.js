095031a488ec187bb3ac99df47fc0faafa97e09f
YaswanthGotham <Yaswanth.Gotham@thoughtworks.com>
2021-01-14 08:45:47 +0530

    corrected test case

--- src/minimum.spec.js
+++ src/minimum.spec.js
@@ -3,7 +3,7 @@ const minimum = require('./minimum');
 describe('minimum', () => {
 
     it('minimum of 1, 2, 3, 4 is 1', () => {
-        expect(minimum([0, 0])).toEqual(0);
+        expect(minimum([1, 2, 3, 4])).toEqual(1);
     });
 
 })
